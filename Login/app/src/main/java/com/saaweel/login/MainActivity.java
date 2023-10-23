11b6141848caab8a9e3fd64a679f699e4f607ecc
Saaweel <sawelsports@gmail.com>
2023-10-23 17:45:11 +0200

    .

--- Login/app/src/main/java/com/saaweel/login/MainActivity.java
+++ Login/app/src/main/java/com/saaweel/login/MainActivity.java
@@ -3,6 +3,8 @@ package com.saaweel.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.loginButton).setOnClickListener(e -> {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new Login()).commit();
+            findViewById(R.id.fragmentContainer).setVisibility(View.VISIBLE);
+        });
+
+        findViewById(R.id.registerButton).setOnClickListener(e -> {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new Register()).commit();
+            findViewById(R.id.fragmentContainer).setVisibility(View.VISIBLE);
+        });
     }
 }
