49eee0e8d29bed343b971bb762d7533008fb8e7a
Saaweel <sawelsports@gmail.com>
2023-11-06 18:29:33 +0100

    .

--- instadam/app/build.gradle.kts
+++ instadam/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.saaweel.instadam"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.saaweel.instadam"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- instadam/app/src/main/java/com/saaweel/instadam/MainActivity.java
+++ instadam/app/src/main/java/com/saaweel/instadam/MainActivity.java
@@ -3,12 +3,45 @@ package com.saaweel.instadam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+/** @noinspection ALL*/
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.notifications) {
+            Fragment fragment = new NotificationsFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.primaryView, fragment).commit();
+            return true;
+        }
+
+        if (id == R.id.chats) {
+            Fragment fragment = new ChatsFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.primaryView, fragment).commit();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
