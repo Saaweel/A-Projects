c1f77d8e3cc544540f57f1e7e8767fd96095ae47
Saaweel <sawelsports@gmail.com>
2023-11-14 18:16:06 +0100

    instadam

--- instadam/app/src/main/java/com/saaweel/instadam/MainActivity.java
+++ instadam/app/src/main/java/com/saaweel/instadam/MainActivity.java
@@ -3,45 +3,34 @@ package com.saaweel.instadam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
-/** @noinspection ALL*/
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-    }
+        bottomNavigation = findViewById(R.id.bottomNavigation);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
-        return true;
-    }
+        bottomNavigation.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.startItem) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+            } else if (item.getItemId() == R.id.searchItem) {
 
-        if (id == R.id.notifications) {
-            Fragment fragment = new NotificationsFragment();
-            getSupportFragmentManager().beginTransaction().replace(R.id.primaryView, fragment).commit();
+            } else if (item.getItemId() == R.id.cameraItem) {
+
+            } else if (item.getItemId() == R.id.notifyItem) {
+
+            } else if (item.getItemId() == R.id.profileItem) {
+
+            }
+
             return true;
-        }
-
-        if (id == R.id.chats) {
-            Fragment fragment = new ChatsFragment();
-            getSupportFragmentManager().beginTransaction().replace(R.id.primaryView, fragment).commit();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        });
     }
 }
