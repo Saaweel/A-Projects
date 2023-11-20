52ffc3ff4c1878d8a7f40832994df32fdd2cf3df
Saaweel <sawelsports@gmail.com>
2023-11-20 17:54:34 +0100

    Fragments

--- instadam/app/src/main/java/com/saaweel/instadam/MainActivity.java
+++ instadam/app/src/main/java/com/saaweel/instadam/MainActivity.java
@@ -1,8 +1,10 @@
 package com.saaweel.instadam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -10,24 +12,38 @@ import com.google.android.material.navigation.NavigationBarView;
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigation;
 
+    /*
+     * Este método se encarga de cambiar el fragmento que se muestra en pantalla
+     * @param fragment El fragmento que se va a mostrar
+     * @return void
+     */
+    private void changeFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.mainActivity, fragment).commit();
+        findViewById(R.id.mainActivity).setVisibility(View.VISIBLE);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Obtenemos el menú de navegación
         bottomNavigation = findViewById(R.id.bottomNavigation);
 
+        // Cargamos de primera el fragmento de inicio
+        changeFragment(new Start());
+
+        // En base a la opción elegida cambiamos al fragmento necesario
         bottomNavigation.setOnItemSelectedListener(item -> {
             if (item.getItemId() == R.id.startItem) {
-
+                changeFragment(new Start());
             } else if (item.getItemId() == R.id.searchItem) {
-
+                changeFragment(new Search());
             } else if (item.getItemId() == R.id.cameraItem) {
-
+                changeFragment(new Camera());
             } else if (item.getItemId() == R.id.notifyItem) {
-
+                changeFragment(new Notify());
             } else if (item.getItemId() == R.id.profileItem) {
-
+                changeFragment(new Profile());
             }
 
             return true;
---
+++ instadam/app/src/main/java/com/saaweel/instadam/Start.java
@@ -0,0 +1,24 @@
+package com.saaweel.instadam;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Start extends Fragment {
+    public Start() {
+        // Se necesita un constructor vacío
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_start, container, false);
+    }
+}
