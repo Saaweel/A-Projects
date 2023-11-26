1c2f77f3065696a2b3260c6b6add91002b28ca68
Saaweel <sawelsports@gmail.com>
2023-11-26 03:35:47 +0100

    .

--- instadam/app/src/main/java/com/saaweel/instadam/views/mini_post/MiniPost.java
+++ instadam/app/src/main/java/com/saaweel/instadam/views/mini_post/MiniPost.java
@@ -1,6 +1,10 @@
 package com.saaweel.instadam.views.mini_post;
 
+import android.app.Activity;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,6 +20,19 @@ public class MiniPost extends RecyclerView.ViewHolder {
         super(view);
 
         image = view.findViewById(R.id.image_mini_post);
+
+        FrameLayout layout = view.findViewById(R.id.layout_mini_post);
+
+        ViewGroup.LayoutParams layoutParams = layout.getLayoutParams();
+
+        DisplayMetrics metrics = new DisplayMetrics();
+
+        ((Activity) view.getContext()).getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        layoutParams.width = metrics.widthPixels / 3;
+        layoutParams.height = metrics.widthPixels / 3;
+
+        layout.setLayoutParams(layoutParams);
+
     }
 
     public void setPostData(Post post) {
