2042b28a5c860c49f212bb8e51da304b2f5985a2
Saúl Díaz <sauldiazdiaz210@gmail.com>
2024-05-07 20:08:23 +0200

    Logos

---
+++ HealthCheckAI/app/src/main/java/com/saaweel/healthcheckai/activities/ScreenLoadingActivity.java
@@ -0,0 +1,16 @@
+package com.saaweel.healthcheckai.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.saaweel.healthcheckai.R;
+
+public class ScreenLoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_loading);
+    }
+}
